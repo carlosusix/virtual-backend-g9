f6915541efb741c4b88be2b3a526c7b651092813
carlosusix <36808028+carlosusix@users.noreply.github.com>
2021-08-16 21:31:07 -0500

    chore:Agregue todos los archivos de trabajo

---
+++ Dia 1/02-impresion-pantalla.py
@@ -0,0 +1,16 @@
+mes="agosto"
+print("el mes es:", mes)
+
+dia=16
+print("el dia", dia, "del mes", mes)
+
+print("Los alumnos son {} y son del grupo {} y todos están atentos".format('Carlos',10.5))
+#dentro de las llaves va codigo de python
+alumnos = 23
+grupo = 9 #las variables numéricas no pueden comenzar con cero 
+print("Los alumnos son {} y son del grupo {} y todos están atentos".format(alumnos,grupo))
+
+print("Los alumnos son {1} y son del grupo {0} y todos están atentos".format(grupo, alumnos))
+
+#cuando ponemos la letra f en el print dentro de las llaves debe ir codigo python
+print(f"los alumnos {dia}")
