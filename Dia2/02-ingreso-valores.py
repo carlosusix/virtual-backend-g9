2e36394020063dcb4790816507ddbd3e7e747a8b
carlosusix <36808028+carlosusix@users.noreply.github.com>
2021-08-17 20:20:17 -0500

    chore: operadores aritmeticos y de comparacion

---
+++ Dia2/02-ingreso-valores.py
@@ -0,0 +1,7 @@
+# input sirve para capturar valores, se almacena en la variable resultado
+resultado = input("Ingresa tu nombre:")
+print(resultado)
+
+print(type(resultado))  # para saber que la variable resulta 
+
+print(int(resultado)) #para ingresar un calor y convertirlo a entero
